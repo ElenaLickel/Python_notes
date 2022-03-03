b0d59eea6adf196b72f826db1136dd18ba71bb14
Sebastian-Ballesteros <97686942+Sebastian-Ballesteros@users.noreply.github.com>
2022-03-03 17:54:37 +0100

    Exercises in class march 3d

---
+++ While_Example.py
@@ -0,0 +1,58 @@
+a = 10
+b = 5
+index = 0
+while True:
+    index = int(input("what example do you want to run"))
+
+# Normal while loop
+    if index == 1:
+        while a > b:
+            print("the first one is greater")
+            b += 1
+
+# While True Loop
+    elif index == 2:
+        add = 0
+
+        while True:
+            number = int(input("give me a number(0 to stop)"))
+            add += number
+            if number == 0:
+                print("the sum is ",add)
+                break
+
+# for loop
+    elif index == 3:
+        greeting = "hello, how are you?"
+
+        for letter in greeting:
+            print(letter*2, end="")
+        print()
+        print(2*greeting)
+
+# splitting strings
+# 1 through 6
+        print(greeting[1:6])
+# 1 through end of string
+        print(greeting[1:])
+#
+        print(greeting[2::2])
+
+        print(greeting[::-1])
+#
+    elif index == 4:
+        text = "a nut for a jar of tuna"
+        text = text.replace(" ", "")
+        print(text)
+
+        reversed = text[::-1]
+        print(reversed)
+        if text == reversed:
+            print("this is a palindrome")
+        else:
+            print("this is not a palindrome")
+
+# end of examples
+    elif index == 0:
+        print("see you later")
+        break
---
+++ if example.py
@@ -0,0 +1,9 @@
+a = 10
+b = 5
+if a > b:
+    print("the first number is bigger")
+elif a == b:
+    print("They are equal")
+else:
+    print("the first number is smaller")
+
