616884309dbee9436d32c80a36fca401d91e592a
Sebastian-Ballesteros <97686942+Sebastian-Ballesteros@users.noreply.github.com>
2022-03-03 18:31:38 +0100

    Exercises in class march 3d

--- While_Example.py
+++ While_Example.py
@@ -53,6 +53,20 @@ while True:
         else:
             print("this is not a palindrome")
 
+    elif index ==5:
+        try:
+            age = input("How old are you? ")
+            print("in 2032 you will be ", int(age) + 10)
+            print(100 / int(age))
+
+        except ValueError:
+            print("Hey, that is not a number")
+        except ZeroDivisionError:
+            print("Hey, you are dividing by zero")
+        finally:
+            print("This is what we do after all this is done")
+            print("This will print no matter what the code above is")
+
 # end of examples
     elif index == 0:
         print("see you later")
