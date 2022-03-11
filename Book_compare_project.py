392fe21c56d46832d63673ca48f67884feda1db5
Sebastian-Ballesteros <97686942+Sebastian-Ballesteros@users.noreply.github.com>
2022-03-11 14:40:28 +0100

    Working forloop with changing url

--- Book_compare_project.py
+++ Book_compare_project.py
@@ -1,33 +1,38 @@
 from urllib.request import urlopen
 
+
+def get_unique_words():
+    punctuation = ",.?!-+"
+    unique_words = {}
+    word_count = 0
+    with open(str(local_name)) as fp:
+        for line in fp:
+            # remove punctuation
+            for p in punctuation:
+                line.replace(p, " ")
+            line = line.lower()
+            for word in line.split():
+                unique_words[word] = unique_words.get(word, 0) + 1
+                word_count += 1
+    return unique_words, word_count
+
 books = {
-    "Wealth:_of_Nations.txt": "https://www.gutenberg.org/files/3300/3300-h/3300-h.htm",
-    "Communist_Manifesto.txt": "https://www.gutenberg.org/files/61/61-h/61-h.htm"
 }
 
-for local_name, url in books.items():
+for local_name in range(9, 1000):
     print(local_name)
-    with open(local_name, "w") as local_fp:
-        with urlopen(url) as fp:
-            for line in fp:
-                line = line.decode().replace("\n", "")
-                local_fp.write(line)
+    with open(str(local_name), "w") as local_fp:
+        try:
+            with urlopen(f"https://www.gutenberg.org/files/{local_name}/{local_name}-h/{local_name}-h.htm") as fp:
+                for line in fp:
+                    line = line.decode().replace("\n", "")
+                    local_fp.write(line)
 
-    punctuation = ",.?!-+"
-
-    def get_unique_words():
-        unique_words = {}
-        word_count = 0
-        with open(local_name) as fp:
-            for line in fp:
-                # remove punctuation
-                for p in punctuation:
-                    line.replace(p, " ")
-                line = line.lower()
-                for word in line.split():
-                    unique_words[word] = unique_words.get(word, 0) + 1
-                    word_count += 1
-        return unique_words, word_count
+                    if str(line[0:2]) == "The Project Gutenberg":
+                        print("hello")
+        except:
+            print("error")
+            continue
 
     #save_locally()
     unique_count = []
@@ -35,7 +40,7 @@ for local_name, url in books.items():
     unique_words, word_count = get_unique_words()
     most_frequent = list(unique_words.values())
     most_frequent.sort(reverse=True)
-    print(most_frequent)
+    print(most_frequent[:5])
 
     unique_count.append(len(unique_words.keys()))
 
---
+++ test.py
@@ -0,0 +1,4 @@
+f"https://www.gutenberg.org/files/3300/3300-h/3300-h.htm"
+
+for i in range(9, 1000):
+    print(f"https://www.gutenberg.org/files/{i}/{i}-h/{i}-h.htm")
