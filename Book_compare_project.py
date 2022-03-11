e8ba59c4b3a646ed6ad09b0bb585104c89ddcf49
Sebastian-Ballesteros <97686942+Sebastian-Ballesteros@users.noreply.github.com>
2022-03-11 12:55:48 +0100

    for loop integrated

--- Book_compare_project.py
+++ Book_compare_project.py
@@ -5,36 +5,52 @@ books = {
     "Communist_Manifesto.txt": "https://www.gutenberg.org/files/61/61-h/61-h.htm"
 }
 
+for local_name, url in books.items():
+    print(local_name)
+    with open(local_name, "w") as local_fp:
+        with urlopen(url) as fp:
+            for line in fp:
+                line = line.decode().replace("\n", "")
+                local_fp.write(line)
 
-#for local_name, url in books:
+    punctuation = ",.?!-+"
 
-local_name = "Wealth:_of_Nations.txt"
-url = "https://www.gutenberg.org/files/3300/3300-h/3300-h.htm"
-with open(local_name, "w") as local_fp:
-    with urlopen(url) as fp:
-        for line in fp:
-            line = line.decode().replace("\n", "")
-            local_fp.write(line)
-punctuation = ",.?!-+"
-def get_unique_words():
-    unique_words = {}
-    with open(local_name) as fp:
-        for line in fp:
-            # remove punctuation
-            for p in punctuation:
-                line.replace(p, " ")
-            line = line.lower()
-            for word in line.split():
-                unique_words[word] = unique_words.get(word, 0) + 1
-    return unique_words
+    def get_unique_words():
+        unique_words = {}
+        word_count = 0
+        with open(local_name) as fp:
+            for line in fp:
+                # remove punctuation
+                for p in punctuation:
+                    line.replace(p, " ")
+                line = line.lower()
+                for word in line.split():
+                    unique_words[word] = unique_words.get(word, 0) + 1
+                    word_count += 1
+        return unique_words, word_count
 
-#save_locally()
-unique_words= get_unique_words()
-most_frequent = list(unique_words.values())
-most_frequent.sort(reverse=True)
-print(most_frequent)
-for word in most_frequent[0:10]:
-    for unique_word, value in unique_words.items():
-        if word == value:
-            print(f"common word {unique_word} appears {value} times")
+    #save_locally()
+    unique_count = []
+    unique_7 =[]
+    unique_words, word_count = get_unique_words()
+    most_frequent = list(unique_words.values())
+    most_frequent.sort(reverse=True)
+    print(most_frequent)
+
+    unique_count.append(len(unique_words.keys()))
+
+    print(unique_count)
+
+    for i in unique_words.keys():
+        if len(i) >= 7:
+            unique_7.append(i)
+    unique_7 = len(unique_7)
+    print(unique_7)
+
+    print(unique_count[0]/word_count)
+
+
+#for word in most_frequent[0:10]:
+    #for unique_word, value in unique_words.items():
+
 
